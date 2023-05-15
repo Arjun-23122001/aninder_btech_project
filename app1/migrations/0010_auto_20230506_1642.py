85062eed4fa56d89302af59a3d1aaf2fbe5031dd
ARJUN T <83713729+Arjun-23122001@users.noreply.github.com>
2023-05-15 09:32:23 +0530

    Update 0010_auto_20230506_1642.py

--- app1/migrations/0010_auto_20230506_1642.py
+++ app1/migrations/0010_auto_20230506_1642.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
             name='camera',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('Video', models.TextField()),
+                ('CAM_ID', models.TextField()),
                 ('Latitude', models.FloatField()),
                 ('Longitude', models.FloatField()),
                 ('Location', models.TextField()),
