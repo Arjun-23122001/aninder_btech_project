34ee18feccb4f94a19e86b82ae81f8bcd32a1480
ARJUN T <83713729+Arjun-23122001@users.noreply.github.com>
2023-05-15 09:42:01 +0530

    Delete app1 directory

--- app1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.db import models
-from django.contrib.admin import AdminSite
-from django.http import HttpResponse
-from django.shortcuts import redirect
-from django.template import loader
-from django.urls import reverse
-# Register your models here.
-
